17cdf4f6c531a3ba0120c834466bc39adafcf00c
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-11-01 01:11:11 -0300

    Add a new layer for message error

--- gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestaoDeVagas-master/src/main/java/br/com/patrick/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,12 +1,14 @@
 package br.com.patrick.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.patrick.gestao_vagas.exceptions.UserFoundException;
+import br.com.patrick.gestao_vagas.modules.UseCases.CreateCandidateUseCase;
 import br.com.patrick.gestao_vagas.modules.candidate.CandidateEntity;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +20,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CandidateController {
     
-
+    @Autowired
+    private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent((c) -> {
-            throw new UserFoundException();
-        });
-        return this.candidateRepository.save(candidateEntity);
-}
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
